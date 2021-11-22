bae863fb9bc03344e26a4c62cd4c5246160186c5
SeaWaveW <1170876169@qq.com>
2021-11-22 18:06:12 +0800

    树生成添加树叶生成时的回调

--- src/methods/tree.js
+++ src/methods/tree.js
@@ -1,29 +1,29 @@
 /**
      * @method 生成树型结构
-     * @param {Array,String,String,String,Array} 
-     * @desc  数据树（第一次则为源数据），树构成, 子赖父标识， 为父根据, 源数据
+     * @param {Array,String,String,String,Array or Function,Function} 
+     * @desc  数据树（第一次则为源数据），树构成, 子赖父标识， 为父根据, 源数据(若为第一次则为回调函数)，回调函数
      * @returns {Object}
      */
-const createdTree = (list = [], key = 'children', child = 'parentId', parent = 'id',oldList = []) => {
+const createdTree = (list = [], key = 'children', child = 'parentId', parent = 'id',oldList,callBack) => {
     let parentList = []
     // 若是第一次则进行生成祖宗节点
-    if(!oldList.length){
+    if(typeof oldList === 'function' || (Array.isArray(oldList) && !oldList.length)){
         // 若是无依赖标识则为祖宗节点
         parentList = list.filter(item => !item[child])
     }else {
         parentList = list
     }
     // 赋值
-    const newList = oldList.length ? oldList : list
+    const newList = Array.isArray(oldList) && oldList.length ? oldList : list
     // 进行父级循环
     parentList.forEach(item => {
         // 若数组还有剩余则继续生成子项
         if(newList.length){
-            item[key] = createdChildren(newList,item,child,parent)
+            item[key] = createdChildren(newList,item,child,parent, typeof oldList === 'function' ? oldList : callBack)
         }
         // 再次进行判断，是否存在数构成项,若是存在则继续递归生成
         if(item[key] && item[key].length){
-            item[key] = createdTree(item[key],key,child,parent,newList)
+            item[key] = createdTree(item[key],key,child,parent,newList, typeof oldList === 'function' ? oldList : callBack)
         }
     })
     // 最后返回
@@ -31,14 +31,18 @@ const createdTree = (list = [], key = 'children', child = 'parentId', parent = '
 }
 /**
  * @method 生成树叶
- * @param {Array,Object,String,String} 
- * @desc  数据树，当前项, 子赖父标识， 为父根据
+ * @param {Array,Object,String,String,Function} 
+ * @desc  数据树，当前项, 子赖父标识， 为父根据, 回调函数
  * @returns {Object}
  */
-const createdChildren = ( list = [], obj = {}, child = 'parentId', parent = 'id')  => {
+const createdChildren = ( list = [], obj = {}, child = 'parentId', parent = 'id',callBack)  => {
     const newList = []
     list.forEach((item) => {
         if(item[child] === obj[parent]){
+            // 当有回调函数则先执行回调
+            if(typeof callBack === 'function'){
+                callBack(obj,item)
+            }
             // 添加为子项
             newList.push({
                 ...item,
